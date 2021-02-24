29b802b2475aeeceef0ca7dba12cb87d85dc9d37
jorgew1998 <66254573+jorgew1998@users.noreply.github.com>
2021-02-24 13:07:30 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import Movies from "./ListMovies";
+import React, { useEffect, useState } from "react";
+import { Input, Space, Row, Col } from 'antd';
+import { AudioOutlined } from '@ant-design/icons';
+
+
 
 function App() {
+
+  document.title = "List of Movies";
+  const [movies, setMovies] = useState([]);
+  const [elementMovie, setElementMovie] = useState('avengers');
+
+  const { Search } = Input;
+  const suffix = (
+    <AudioOutlined
+      style={{
+        fontSize: 16,
+        color: '#1890ff',
+      }}
+    />
+  );
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch(`http://www.omdbapi.com/?apikey=537fcb7&s=${elementMovie}`);
+      const dataMovies = await response.json();
+      setMovies(dataMovies.Search);
+    }
+    getData();
+
+  }, [elementMovie]);
+
+  const onSearch = (value) => {
+    setElementMovie(value);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Row>
+        <Col span={10}></Col>
+        <Col span={14}><Space direction="vertical">
+          <Search
+            placeholder="Nombre "
+            enterButton="Search"
+            size="large"
+            suffix={suffix}
+            onSearch={onSearch}
+
+          />
+        </Space></Col>
+      </Row>
+      <Row>
+        <Col span={2}></Col>
+        <Col span={22}><Movies movies={movies} /></Col>
+      </Row>
+    </>
   );
 }
 
